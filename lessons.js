93d46314bf38b7b20c6f34589d22ee04021a7445
Dulasi Nethma <149291478+Dula21@users.noreply.github.com>
2024-11-27 16:24:57 +0400

    added lessons.js

---
+++ lessons.js
@@ -0,0 +1,115 @@
+const lessons = [
+    {
+        id: 1,
+        title: "Maths",
+        description: "Enhance your math skills!",
+        price: 100,
+        availableInventory: 5,
+        image: "Images/maths.png",
+        alt: "Math Lesson",
+        location: "London" ,// New location property
+        rating: 4 // New rating property
+    },
+    
+    {
+        id: 2,
+        title: "Arabic",
+        description: "Enhance your Arabic skills!",
+        price: 130,
+        availableInventory: 5,
+        image: "Images/arabic.png",
+        alt: "arabic Lesson",
+        location: "California" ,// New location property
+        rating: 4 // New rating property
+    // Add more lessons as needed
+       
+    },
+    {
+    id: 3,
+    title: " Physics",
+    description: "Hands-on science experience.",
+    price: 120,
+    availableInventory: 6,
+    image: "Images/science.png",
+    alt: "Physics",
+    location: "Dubai" ,// New location property
+    rating: 5 // New rating property
+    },
+    {
+        id: 4,
+        title: "French",
+        description: "Enhance your french skills!",
+        price: 140,
+        availableInventory: 5,
+        image: "Images/french.png",
+        alt: "french Lesson",
+        location: "Seattle" ,// New location property
+        rating: 3 // New rating property
+    },
+    {
+        id: 5,
+        title: "Chinese",
+        description: "Enhance your Chinese skills!",
+        price: 150,
+        availableInventory: 5,
+        image: "Images/chinese.png",
+        alt: "french Lesson",
+        location: "Chicago" ,// New location property
+        rating: 3 // New rating property
+    },
+    {
+        id: 6,
+        title: "Biology",
+        description: "Enhance your bio skills!",
+        price: 200,
+        availableInventory: 5,
+        image: "Images/biology.png",
+        alt: "french Lesson",
+        location: "Zurich" ,// New location property
+        rating: 3 // New rating property
+    },
+    {
+        id: 7,
+        title: "Chemistry",
+        description: "Enhance your chemistry skills!",
+        price: 400,
+        availableInventory: 5,
+        image: "Images/chemistry.png",
+        alt: "french Lesson",
+        location: "Seattle" ,// New location property
+        rating: 3 // New rating property
+    },
+    {
+        id: 8,
+        title: "History",
+        description: "Aware About old Generation!",
+        price: 600,
+        availableInventory: 5,
+        image: "Images/history.png",
+        alt: "History Lesson",
+        location: "California" ,// New location property
+        rating: 3 // New rating property
+    },
+    {
+        id: 9,
+        title: "Social Studies",
+        description: "Aware About Socitey!",
+        price: 800,
+        availableInventory: 5,
+        image: "Images/social-media.png",
+        alt: "Social Studies",
+        location: "Chicago" ,// New location property
+        rating: 3 // New rating property
+    },
+    {
+        id: 10,
+        title: "Trade and Exports",
+        description: "Enhance your Trading skills!",
+        price: 1100,
+        availableInventory: 5,
+        image: "Images/trade.png",
+        alt: "french Lesson",
+        location: "Dubai" ,// New location property
+        rating: 3 // New rating property
+    }
+];
