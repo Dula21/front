89709c50c6cffb36e3fec70993916048c97e577f
Dula21 <st20259983@outlook.cardiffmet.ac.uk>
2024-11-28 18:48:06 +0400

    made some changes

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const path = require("path");
+//const helmet = require("helmet"); // For security headers
+//const morgan = require("morgan"); // For logging
+//const cors = require("cors"); // For CORS support
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve the 'static' folder
-app.use('/static', express.static(path.join(__dirname, 'static')));
+// Middleware
+//app.use(helmet()); // Set security headers
+//app.use(morgan('combined')); // Log requests
+//app.use(cors()); // Enable CORS
+app.use('/static', express.static(path.join(__dirname, 'static'))); // Serve static files
 
 // Serve index.html
 app.get('/', (req, res) => {
@@ -15,6 +21,12 @@ app.get('/', (req, res) => {
 // Catch-all for other routes (for SPAs or client-side routing)
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 // Start the server
